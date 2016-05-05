224618130c0a5004c483274d19d3b8c8a9b1ce1b
Matt Edelman <medelman@paypal.com>
2016-05-05 11:35:28 -0700

    modify to use bundalo@1.0.0-rc.1

--- controllers/cart/index.js
+++ controllers/cart/index.js
@@ -1,85 +1,74 @@
 'use strict';
 var Product = require('../../models/productModel');
 var getBundle = require('../../lib/getBundle');
-var normalocator = require('../../lib/normalocator');
 module.exports = function (router) {
 
-	/**
-	 * Display the shopping cart
-	 */
-	router.get('/', getBundle, function (req, res) {
+    /**
+     * Display the shopping cart
+     */
+    router.get('/', getBundle, function (req, res) {
 
-		//Retrieve the shopping cart from memory
-		var cart = req.session.cart,
-			displayCart = {items: [], total: 0},
-			total = 0;
-		var locals = res.locals;
-		var i18n = res.app.kraken.get('i18n');
-		var locality = normalocator.toStr(locals && locals.context && locals.context.locality || i18n.fallback);
-		var cartLength = 0;
-		if (!cart) {
-			res.bundle.raw.get({'bundle': 'messages', 'locality': locality}, function bundleReturn(err, messages) {
-				if (err) {
-					console.error(err.stack);
-				}
-				res.render('result', {result: messages.get('empty'), continueMessage: messages.get('keepShopping')});
-			});
+        //Retrieve the shopping cart from memory
+        var cart = req.session.cart,
+          displayCart = {items: [], total: 0},
+          total = 0;
 
-			return;
-		}
+        var cartLength = 0;
+        if (!cart) {
+            res.render('result', {result: res.bundle.get('empty'), continueMessage: res.bundle.get('keepShopping')});
+            return;
+        }
 
-		//Ready the products for display
-		for (var item in cart) {
-			cartLength += cart[item].qty;
-			displayCart.items.push(cart[item]);
-			total += (cart[item].qty * cart[item].price);
-		}
-		req.session.total = displayCart.total = total.toFixed(2);
-		var model = {
-			cart: displayCart
-		};
-		res.bundle.fmt.get({'bundle': 'messages', 'locality': locality}, function bundleReturn(err, messages) {
-			model.itemsInCart = messages.get('items').format({cartItemLength: cartLength});
-			res.render('cart', model);
-		});
+        //Ready the products for display
+        for (var item in cart) {
+            cartLength += cart[item].qty;
+            displayCart.items.push(cart[item]);
+            total += (cart[item].qty * cart[item].price);
+        }
+        req.session.total = displayCart.total = total.toFixed(2);
+        var model = {
+            cart: displayCart
+        };
+        model.itemsInCart = res.bundle.getIntl('items', {cartItemLength: cartLength});
+        res.render('cart', model);
 
-	});
+    });
 
-	/**
-	 * Add an item to the shopping cart
-	 */
-	router.post('/', function (req, res) {
+    /**
+     * Add an item to the shopping cart
+     */
+    router.post('/', function (req, res) {
 
-		//Load (or initialize) the cart
-		req.session.cart = req.session.cart || {};
-		var cart = req.session.cart;
+        //Load (or initialize) the cart
+        req.session.cart = req.session.cart || {};
+        var cart = req.session.cart;
 
-		//Read the incoming product data
-		var id = req.param('item_id');
+        //Read the incoming product data
+        var id = req.param('item_id');
 
-		//Locate the product to be added
-		Product.findById(id, function (err, prod) {
-			if (err) {
-				console.log('Error adding product to cart: ', err);
-				res.redirect('/cart');
-				return;
-			}
+        //Locate the product to be added
+        Product.findById(id, function (err, prod) {
+            if (err) {
+                console.log('Error adding product to cart: ', err);
+                res.redirect('/cart');
+                return;
+            }
 
-			//Add or increase the product quantity in the shopping cart.
-			if (cart[id]) {
-				cart[id].qty++;
-			}
-			else {
-				cart[id] = {
-					name: prod.name,
-					price: prod.price,
-					prettyPrice: prod.prettyPrice(),
-					qty: 1
-				};
-			}
+            //Add or increase the product quantity in the shopping cart.
+            if (cart[id]) {
+                cart[id].qty++;
+            }
+            else {
+                cart[id] = {
+                    name: prod.name,
+                    price: prod.price,
+                    prettyPrice: prod.prettyPrice(),
+                    qty: 1
+                };
+            }
 
-			res.redirect('/cart');
+            res.redirect('/cart');
 
-		});
-	});
+        });
+    });
 };
